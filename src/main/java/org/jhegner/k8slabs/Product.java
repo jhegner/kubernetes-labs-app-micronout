7af9fc40e54b218d3953337e6d7aca7f71595280
Jidlafe Hegner <jidlafehegner@gmail.com>
2025-08-10 01:17:26 -0300

    feat: add mock api for app

---
+++ src/main/java/org/jhegner/k8slabs/Product.java
@@ -0,0 +1,62 @@
+package org.jhegner.k8slabs;
+
+import io.micronaut.serde.annotation.Serdeable;
+
+@Serdeable
+public class Product {
+
+    private String createdAt;
+    private String name;
+    private String avatar;
+    private String id;
+    private String description;
+    private String price;
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/org/jhegner/k8slabs/UsersClient.java
@@ -0,0 +1,12 @@
+package org.jhegner.k8slabs;
+
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.client.annotation.Client;
+import reactor.core.publisher.Flux;
+
+@Client("${mockapi.protocol}://${mockapi.project-secret}.${mockapi.host}/${mockapi.prefix}")
+public interface UsersClient {
+
+    @Get("/users")
+    Flux<Users> getUsers();
+}
